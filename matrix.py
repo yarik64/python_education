8d86ac55a54d0f15176e4bb5b6ad9f64f43e31b4
Anonimous <anonimous@example.com>
2024-12-08 15:00:43 +0400

    argv read

--- matrix.py
+++ matrix.py
@@ -3,7 +3,9 @@
 import os, sys
 
 
-def main():
-    print("Hello! This is matrix!!!")
+def main(argv):
+    print("Hello! This is matrix!!!\n",
+          "ARGV:= ", *argv
+          )
 
-if __name__ == "__main__": main(*sys.args())
+if __name__ == "__main__": main(sys.argv[1:])
