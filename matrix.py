28454ca5a74024103913a63201dcfbdd7c9f8a83
Anonimous <anonimous@example.com>
2024-12-09 12:57:04 +0400

    matrix task working

--- matrix.py
+++ matrix.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
-import os, sys
+import sys
 
 
 def main(argv):
-    print("Hello! This is matrix!!!\n",
-          "ARGV:= ", *argv
-          )
+    print("Hello! This is matrix!!!\nARGV:= ", argv)
+
+    x, y = map(int, argv[:])
+    a = [ i+1 for i in range(x)]
+    for i in range(y):
+        print(*a)
+        a.append(a.pop(0))
+
 
 if __name__ == "__main__": main(sys.argv[1:])
