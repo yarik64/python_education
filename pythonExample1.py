0fbcc1f073be140adad21e16d86ed0eb5a11a419
Anonimous <anonimous@example.com>
2024-12-09 13:02:41 +0400

    code cleanup

--- pythonExample1.py
+++ pythonExample1.py
@@ -3,7 +3,7 @@
 
 # matrix=[ [1,2,3], [4,5,6], [7,8,9] ]
 a=5
-matrix = [ list(range(i*a+1, (i+1)*a+1)) for i in range(a) ] 
+matrix = [ list(range(i*a+1, (i+1)*a+1)) for i in range(a) ]
 
 
 factor      =len(matrix)**2
